2c458895a9d3aba3b5257d8fb6654bea58a2ee2c
Kshitiz Sharma <kshitizsharma2632@gmail.com>
2023-08-19 17:21:34 +0530

    updated cloudinaryconfig

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary=require("cloudinary").v2;
 require("dotenv").config();
 
 
-const cloudConfig = async ()=>{
+const cloudConfig = ()=>{
     try{
-        await cloudinary.config({
+        cloudinary.config({
             cloud_name : process.env.CLOUD_NAME,
             api_key : process.env.API_KEY,
             api_secret : process.env.API_SECRET
